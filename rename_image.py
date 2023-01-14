4c91461cd48edee1f69d51cdf994e5526a0ea32e
Vicky Prahastra <vickyprahastra@gmail.com>
2023-01-14 14:14:03 +0700

    update

--- rename_image.py
+++ rename_image.py
@@ -7,7 +7,6 @@ from pathlib import Path
 import uuid
 
 metadata_dir = 'metadata'
-final_dir = 'final'
 # rename_metadata_dir = 'rename_metadata'
 
 file_range = len(os.listdir(metadata_dir)) # get files range
@@ -21,15 +20,16 @@ def check_or_create_directory(directory):
 for index in range(file_range):
     json_file = open(f'{metadata_dir}/{index}.json')
     data = json.load(json_file)
-    get_uuid = uuid.uuid4().hex
 
     filename = data['image']
-    data['image'] = f'https://shibago.s3.ap-southeast-1.amazonaws.com/{index}-{get_uuid}.gif'
-    print(f'https://shibago.s3.ap-southeast-1.amazonaws.com/{index}-{get_uuid}.gif')
+    # data['image'] = os.path.splitext(filename)[0]+f'-{uuid.uuid4().hex}.gif'
+    print(os.path.splitext(filename)[0]+f'-{uuid.uuid4().hex}.gif')
 
 
-    with open(f'{metadata_dir}/{index}.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-    # filepath = Path(f'{final_dir}/{index}.gif')
-    # filepath.rename(filepath.with_suffix(f'{index}-{get_uuid}.gif'))
-    os.rename(f'{final_dir}/{index}.gif', f'{final_dir}/{index}-{get_uuid}.gif')
+    # with open(f'{metadata_dir}/{index}', 'w', encoding='utf-8') as f:
+    #     json.dump(data, f, ensure_ascii=False, indent=4)
+    #
+    # filepath = Path(f'{metadata_dir}/{index}')
+    # filepath.rename(filepath.with_suffix('.json'))
+    # print(filepath)
+
--- rename_image_host.py
+++ rename_image_host.py
@@ -21,15 +21,13 @@ def check_or_create_directory(directory):
 for index in range(file_range):
     json_file = open(f'{metadata_dir}/{index}.json')
     data = json.load(json_file)
-    get_uuid = uuid.uuid4().hex
 
     filename = data['image']
-    data['image'] = f'https://shibago.s3.ap-southeast-1.amazonaws.com/{index}-{get_uuid}.gif'
-    print(f'https://shibago.s3.ap-southeast-1.amazonaws.com/{index}-{get_uuid}.gif')
+    data['image'] = f'https://ipfs.io/ipfs/QmT8Wm291gwhAXqvALLM8k4S6bEdLTvxuvuKu3x17dUy5F/{os.path.basename(filename)}'
+    print(data['image'])
 
 
     with open(f'{metadata_dir}/{index}.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
     # filepath = Path(f'{final_dir}/{index}.gif')
     # filepath.rename(filepath.with_suffix(f'{index}-{get_uuid}.gif'))
-    os.rename(f'{final_dir}/{index}.gif', f'{final_dir}/{index}-{get_uuid}.gif')
